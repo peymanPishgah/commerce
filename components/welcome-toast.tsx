5388614f192c7cd0f7c763b7554a712c06f3b6c0
p.pishgah <pishgah.peyman@gmail.com>
2024-10-26 19:42:28 +0330

    tost edit

--- components/welcome-toast.tsx
+++ components/welcome-toast.tsx
@@ -8,7 +8,7 @@ export function WelcomeToast() {
     // ignore if screen height is too small
     if (window.innerHeight < 650) return;
     if (!document.cookie.includes('welcome-toast=2')) {
-      toast('🛍️ Welcome to Next.js Commerce!', {
+      toast('🛍️ Welcome', {
         id: 'welcome-toast',
         duration: Infinity,
         onDismiss: () => {
@@ -16,15 +16,15 @@ export function WelcomeToast() {
         },
         description: (
           <>
-            This is a high-performance, SSR storefront powered by Shopify, Next.js, and Vercel.{' '}
+            we help you to have best choice{' '}
+            <br />
             <a
-              href="https://vercel.com/templates/next.js/nextjs-commerce"
+              href="https://"
               className="text-blue-600 hover:underline"
               target="_blank"
             >
-              Deploy your own
+              questions to find your item!
             </a>
-            .
           </>
         )
       });
